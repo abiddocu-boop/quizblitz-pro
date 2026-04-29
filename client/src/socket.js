3d71592a7f975040d04c3a2d8c21f4d1480f1c6b
abiddocu-boop <abid.docu@gmail.com>
2026-04-29 16:21:55 +0600

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,13 @@
 import { io } from 'socket.io-client';
+
 const URL = import.meta.env.VITE_SERVER_URL || '';
+
 const socket = io(URL, {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
-  transports: ['websocket', 'polling'],
+  transports: ['polling', 'websocket'],
 });
+
 export default socket;
