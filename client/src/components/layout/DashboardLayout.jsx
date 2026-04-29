4da8c2fcb6aeff5509326eb3aab087f0b947c88a
abiddocu-boop <abid.docu@gmail.com>
2026-04-29 16:11:59 +0600

    Update DashboardLayout.jsx

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar.jsx';
-export default function DashboardLayout({ children }) {
+
+export default function DashboardLayout() {
   return (
-    <div className="app-layout">
+    <div className="dashboard-layout">
       <Sidebar />
-      <main className="main-content">{children}</main>
+      <main className="main-content">
+        <Outlet />
+      </main>
     </div>
   );
 }
